5d42292223dca76ddbc56952a4d5a4f76c1ebf86
Mairon <maironwicz@gmail.com>
2025-10-04 16:15:26 -0300

    feat: major refactor

--- code/utilities/python_module_test/func.py
+++
@@ -1,8 +0,0 @@
-a = 2
-
-class Func:
-    def __init__(self, inputs):
-        a = inputs
-
-    def run(val):
-        Func(val)
--- code/utilities/python_module_test/main.py
+++
@@ -1,9 +0,0 @@
-import func
-from func import a
-
-print(a)
-print(func.a)
-Func = func.Func(4)
-Func.run(3)
-print(Func.a)
-print(Func.a)

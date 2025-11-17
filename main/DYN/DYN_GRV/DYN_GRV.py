4697e22d4cc8f35e864ac8816c1f164d30f5cf3e
Mairon <maironwicz@gmail.com>
2025-11-17 18:32:25 -0300

    feat: added SUN and MARS effect on gravity

--- main/DYN/DYN_GRV/DYN_GRV.py
+++ main/DYN/DYN_GRV/DYN_GRV.py
@@ -1,5 +1,5 @@
 # Level 2 Module DYN_GRV
-# Simulates the gravity model around a solar system body
+# Simulates the gravity model around multiple solar system bodies
 
 import numpy as np
 from .DYN_GRV_par import DYN_GRV_par
@@ -23,26 +23,50 @@ def outputs(t, DYN_out):
     # Get parameters and states to make code more readable
     gravitational_cst = CONSTANTS_par["gravitational_cst"] # [km^2/kg s^2]
     SCmass_cst        = CONSTANTS_par["SCmass_cst"]        # [kg]
+    SUNmass_cst       = CONSTANTS_par["SUNmass_cst"]       # [kg]
     EARTHmass_cst     = CONSTANTS_par["EARTHmass_cst"]     # [kg]
+    MARSmass_cst      = CONSTANTS_par["MARSmass_cst"]      # [kg]
+    SCpos_SUN         = DYN_out["DYN_TRA"]["SCpos_SUN"]    # [km]
     SCpos_TER         = DYN_out["DYN_TRA"]["SCpos_TER"]    # [km]
-    grvacc_TER        = DYN_out["DYN_GRV"]["grvacc_TER"]   # [km/s^2]
+    SCpos_MAR         = DYN_out["DYN_TRA"]["SCpos_MAR"]    # [km]
 
     # Compute the standard gravitational parameter around each body
+    mu_SUN   = gravitational_cst*(SCmass_cst + SUNmass_cst)   # [km^3/s^2]
     mu_EARTH = gravitational_cst*(SCmass_cst + EARTHmass_cst) # [km^3/s^2]
+    mu_MARS  = gravitational_cst*(SCmass_cst + MARSmass_cst)  # [km^3/s^2]
 
-    # Compute the point mass acceleration (no perturbation) in the body rotative frame
-    grvacc_TER = -mu_EARTH * SCpos_TER/np.linalg.norm(SCpos_TER)**3 # [km/s^2]
+    # Compute the point mass acceleration (no perturbation) in each body rotative frame
+    grvacc_SUN_SUN   = -mu_SUN   * SCpos_SUN/np.linalg.norm(SCpos_SUN)**3 # [km/s^2]
+    grvacc_EARTH_TER = -mu_EARTH * SCpos_TER/np.linalg.norm(SCpos_TER)**3 # [km/s^2]
+    grvacc_MARS_MAR  = -mu_MARS  * SCpos_MAR/np.linalg.norm(SCpos_MAR)**3 # [km/s^2]
 
-    # Compute the gravity acceleration in the body inertial frame
+    # Compute the gravity acceleration in each body inertial frame
+    SSBq_SUN = DYN_out["DYN_SUN"]["SSBq_SUN"]
     ECIq_TER = DYN_out["DYN_EARTH"]["ECIq_TER"]
-    grvacc_ECI = quaternions.qvecrot(grvacc_TER, ECIq_TER)
+    MCIq_MAR = DYN_out["DYN_MARS"]["MCIq_MAR"]
+    grvacc_SUN_SSB   = quaternions.qvecrot(grvacc_SUN_SUN, SSBq_SUN)
+    grvacc_EARTH_ECI = quaternions.qvecrot(grvacc_EARTH_TER, ECIq_TER)
+    grvacc_MARS_MCI  = quaternions.qvecrot(grvacc_MARS_MAR, MCIq_MAR)
 
-    # Compute the gravity acceleration in the SSB frame
-    grvacc_SSB = grvacc_ECI
+    # Compute the gravity acceleration in the SSB frame (add all inertial models together)
+    grvacc_SSB = grvacc_SUN_SSB + grvacc_EARTH_ECI + grvacc_MARS_MCI
+    grvacc_ECI = grvacc_SSB
+    grvacc_MCI = grvacc_SSB
 
-    DYN_out["DYN_GRV"]["grvacc_TER"] = grvacc_TER # [km/s^2]
-    DYN_out["DYN_GRV"]["grvacc_ECI"] = grvacc_ECI # [km/s^2]
+    # Convert back the full model to each reference system
+    SUNq_SSB = DYN_out["DYN_SUN"]["SUNq_SSB"]
+    TERq_ECI = DYN_out["DYN_EARTH"]["TERq_ECI"]
+    MARq_MCI = DYN_out["DYN_MARS"]["MARq_MCI"]    
+    grvacc_SUN = quaternions.qvecrot(grvacc_SSB, SUNq_SSB)
+    grvacc_TER = quaternions.qvecrot(grvacc_ECI, TERq_ECI)
+    grvacc_MAR = quaternions.qvecrot(grvacc_MCI, MARq_MCI)
+
     DYN_out["DYN_GRV"]["grvacc_SSB"] = grvacc_SSB # [km/s^2]
+    DYN_out["DYN_GRV"]["grvacc_ECI"] = grvacc_ECI # [km/s^2]
+    DYN_out["DYN_GRV"]["grvacc_MCI"] = grvacc_MCI # [km/s^2]
+    DYN_out["DYN_GRV"]["grvacc_SUN"] = grvacc_SUN # [km/s^2]
+    DYN_out["DYN_GRV"]["grvacc_TER"] = grvacc_TER # [km/s^2]
+    DYN_out["DYN_GRV"]["grvacc_MAR"] = grvacc_MAR # [km/s^2]
 
     return DYN_out
 

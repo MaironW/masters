792d13b48d33852106e9e0d3e8b8d8d8faa26b23
Mairon <maironwicz@gmail.com>
2026-01-01 10:51:25 -0300

    feat: DYN_STR implemented

--- main/DYN/DYN_STR/DYN_STR.py
+++ main/DYN/DYN_STR/DYN_STR.py
@@ -2,8 +2,10 @@
 # Simulates the propagation of the Star position relative to the SSB frame
 
 import copy
+import numpy as np
 
-import spiceypy
+from skyfield.api import load, Star
+from skyfield.data import hipparcos
 
 from Utils import spice
 from Utils.level2module import Level2Module
@@ -17,13 +19,30 @@ class DYN_STR(Level2Module):
         if par_override is not None:
             par.update(par_override)
         # Set initial dummy state
-        self.state = {}
+        self.state = {
+            "STARSpos_SSB" : par["STARSpos_SSB_ini"]
+        }
         super().__init__("DYN_STR", par)
 
     # Initialization
     def initialize(self, states):
-        count = spiceypy.ktotal("STAR")
-        print(count)
+        kernel_dir = "Utils/kernels/"
+        with load.open(kernel_dir + "hip_main.dat") as f:
+            star_df = hipparcos.load_dataframe(f)
+
+        # Filter stars by magnitude
+        star_magnitude_min = self.par["star_magnitude_min"]
+        star_df = star_df[star_df["magnitude"] <= star_magnitude_min]
+
+        # Get star coordinates
+        stars = Star.from_dataframe(star_df)
+        pos   = stars._position_au
+        norm  = np.linalg.norm(pos, axis=0)
+
+        # Update state
+        STARSpos_SSB = pos/norm
+        self.state["STARSpos_SSB"] = STARSpos_SSB
+
         return self.state
 
     # Because the stars are loaded only once, at the start of the simulation,
--- main/DYN/DYN_STR/DYN_STR_par.py
+++ main/DYN/DYN_STR/DYN_STR_par.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 # Parameters for Module DYN_STR
 
 DYN_STR_par = {
-    
+    "STARSpos_SSB_ini" : np.array([0,0,0]), # Initial position of the stars in the SSB frame at the initial epoch
+    "star_magnitude_min" : 3,               # DYN_STR will only select stars with higher brightness than this value
 }

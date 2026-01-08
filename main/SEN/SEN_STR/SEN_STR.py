0c00ac7ce85f844d00b4a26faa9d607c0e42b67f
Mairon <maironwicz@gmail.com>
2026-01-08 08:25:52 -0300

    feat: updates on SEN_STR

--- main/SEN/SEN_STR/SEN_STR.py
+++ main/SEN/SEN_STR/SEN_STR.py
@@ -49,9 +49,12 @@ class SEN_STR(Level2Module):
         time_SIM = DYN_states["DYN_TIME"]["time_SIM"]
         self.update_time_STR(time_SIM)
 
-        # Measured satellite attitude
+        # Satellite attitude
         BOFq_SSB = DYN_states["DYN_ATT"]["BOFq_SSB"]
-        self.update_attitude(BOFq_SSB)
+
+        # Star tracker attitude
+        STRq_BOF = self.par["STRq_BOF"]
+        STRq_SSB = quaternions.qprod(STRq_BOF, BOFq_SSB)
 
         # Positions relative to SSB
         SCpos_SSB     = DYN_states["DYN_TRA"]["SCpos_SSB"]
@@ -61,9 +64,6 @@ class SEN_STR(Level2Module):
         MARSpos_SSB   = DYN_states["DYN_MARS"]["MARSpos_SSB"]
         DEIMOSpos_SSB = DYN_states["DYN_MARS"]["DEIMOSpos_SSB"]
         PHOBOSpos_SSB = DYN_states["DYN_MARS"]["PHOBOSpos_SSB"]
-        
-        # Stars direction relative to the SSB
-        STARSdir_SSB  = DYN_states["DYN_STR"]["STARSdir_SSB"]
 
         # Get positions relative to the Spacecraft, expressed in SSB
         SUNpos_SC    = SUNpos_SSB    - SCpos_SSB
@@ -73,17 +73,47 @@ class SEN_STR(Level2Module):
         DEIMOSpos_SC = DEIMOSpos_SSB - SCpos_SSB
         PHOBOSpos_SC = PHOBOSpos_SSB - SCpos_SSB
 
-        # Get stars directions relative to the Spacecraft
+        # Directions relative to SC, expressed in the SSB frame
+        SUNdir_SC    = self.dir_from_pos(SUNpos_SC)
+        EARTHdir_SC  = self.dir_from_pos(EARTHpos_SC)
+        MOONdir_SC   = self.dir_from_pos(MOONpos_SC)
+        MARSdir_SC   = self.dir_from_pos(MARSpos_SC)
+        DEIMOSdir_SC = self.dir_from_pos(DEIMOSpos_SC)
+        PHOBOSdir_SC = self.dir_from_pos(PHOBOSpos_SC)
+
+        # Stars direction relative to the SSB
+        STARSdir_SSB  = DYN_states["DYN_STR"]["STARSdir_SSB"]
+
+        # Stars directions relative to the Spacecraft
         STARSdir_SC  = STARSdir_SSB
 
-        # Rotate to BOF frame
-        self.state["SUNdir_BOF_mes"]    = self.SCpos_to_BOFdir(SUNpos_SC)
-        self.state["EARTHdir_BOF_mes"]  = self.SCpos_to_BOFdir(EARTHpos_SC)
-        self.state["MOONdir_BOF_mes"]   = self.SCpos_to_BOFdir(MOONpos_SC)
-        self.state["MARSdir_BOF_mes"]   = self.SCpos_to_BOFdir(MARSpos_SC)
-        self.state["DEIMOSdir_BOF_mes"] = self.SCpos_to_BOFdir(DEIMOSpos_SC)
-        self.state["PHOBOSdir_BOF_mes"] = self.SCpos_to_BOFdir(PHOBOSpos_SC)
-        self.state["STARSdir_BOF_mes"]  = self.SCpos_to_BOFdir(STARSdir_SC)
+        # Rotate to STR frame
+        SUNdir_STR    = quaternions.qvecrot(SUNdir_SC,    STRq_SSB)
+        EARTHdir_STR  = quaternions.qvecrot(EARTHdir_SC,  STRq_SSB)
+        MOONdir_STR   = quaternions.qvecrot(MOONdir_SC,   STRq_SSB)
+        MARSdir_STR   = quaternions.qvecrot(MARSdir_SC,   STRq_SSB)
+        DEIMOSdir_STR = quaternions.qvecrot(DEIMOSdir_SC, STRq_SSB)
+        PHOBOSdir_STR = quaternions.qvecrot(PHOBOSdir_SC, STRq_SSB)
+        STARSdir_STR  = quaternions.qvecrot(STARSdir_SC,  STRq_SSB)
+
+        # Filter out objects outside of the FOV
+        self.state["SUNdir_STR"]    = self.filter_visible_objects(SUNdir_STR)
+        self.state["EARTHdir_STR"]  = self.filter_visible_objects(EARTHdir_STR)
+        self.state["MOONdir_STR"]   = self.filter_visible_objects(MOONdir_STR)
+        self.state["MARSdir_STR"]   = self.filter_visible_objects(MARSdir_STR)
+        self.state["DEIMOSdir_STR"] = self.filter_visible_objects(DEIMOSdir_STR)
+        self.state["PHOBOSdir_STR"] = self.filter_visible_objects(PHOBOSdir_STR)
+        self.state["STARSdir_STR"]  = self.filter_visible_objects(STARSdir_STR)
+
+        # Compute noise quaternion
+
+        # Apply noise to each direction vector
+
+        # Convert back to SSB
+
+        # Compute BOFq_SSB_mes
+
+        # NAV_CeleNav will receive STARSdir_SSB_mes + BODYdir_SSB_mes
 
         return self.state
 
@@ -93,13 +123,6 @@ class SEN_STR(Level2Module):
         time_STR = np.floor(time_SIM/dt) * dt
         self.state["time_STR"] = time_STR
 
-    # Update the spacecraft attitude measurement relative to the SSB frame
-    # TODO: Add noise over attitude
-    # TODO: Quanticize
-    def update_attitude(self, BOFq_SSB):
-        BOFq_SSB_mes = BOFq_SSB
-        self.state["BOFq_SSB_mes"] = BOFq_SSB_mes
-
     # Convert a position expressed in SSB to a measured normalized direction in BOF frame
     # TODO: Quanticize
     def SCpos_to_BOFdir(self, pos_SC):
@@ -107,3 +130,13 @@ class SEN_STR(Level2Module):
         dir_BOF_mes  = quaternions.qvecrot(pos_SC, BOFq_SSB_mes)
         dir_BOF_mes  = dir_BOF_mes/np.linalg.norm(dir_BOF_mes, axis=-1, keepdims=True)
         return dir_BOF_mes
+
+    # Return the direction (unitary) vector from a position vector in any given frame
+    def dir_from_pos(self, pos):
+        return pos/np.linalg.norm(pos, axis=-1, keepdims=True)
+
+    def filter_visible_objects(self, dir_STR):
+        STR_boresight = np.array([0, 0, 1])
+        cos_field_of_view = self.par["cos_field_of_view"]
+        visible = (dir_STR @ STR_boresight) >= cos_field_of_view
+        return dir_STR[visible]
--- main/SEN/SEN_STR/SEN_STR_par.py
+++ main/SEN/SEN_STR/SEN_STR_par.py
@@ -1,14 +1,19 @@
 import numpy as np
 from Utils.constants import CONSTANTS_par
 
+# Pre-computation
+field_of_view     = 60*CONSTANTS_par["deg2rad_cst"]
+cos_field_of_view = np.cos(field_of_view)
+
 # Parameters for Module SEN_STR
 SEN_STR_par = {
     # Star Tracker Parameters
-    "dt"            : 0.1,                             # [s] Discretization of the STR time
-    "STRq_BOF"      : np.array([1,0,0,0]),             # Orientation of the STR relative to the Spacecraft body
-    "noise_mean"    : np.array([0,0,0]),               # [rad] STR noise mean in [X,Y,Z] directions
-    "noise_std"     : np.array([0,0,0]),               # [rad] STR noise var in [X,Y,Z] directions
-    "field_of_view" : 60*CONSTANTS_par["deg2rad_cst"], # [rad] STR field of view half-angle
+    "dt"                : 0.1,                 # [s] Discretization of the STR time
+    "STRq_BOF"          : np.array([1,0,0,0]), # Orientation of the STR relative to the Spacecraft body
+    "noise_mean"        : np.array([0,0,0]),   # [rad] STR noise mean in [X,Y,Z] directions
+    "noise_std"         : np.array([0,0,0]),   # [rad] STR noise var in [X,Y,Z] directions
+    "field_of_view"     : field_of_view,       # [rad] STR field of view half-angle
+    "cos_field_of_view" : cos_field_of_view,   # Cosine of the field of view to avoid recomputations later
     # Outputs initial values
     "STRoutflg_ini"       : 0,                          # STR output flag
     "time_STR_ini"        : 0,                          # [s] Star Tracker time

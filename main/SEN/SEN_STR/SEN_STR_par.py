fix: all quaternion functions are now correct - aways passive product

main/SEN/SEN_STR/SEN_STR_par.py
@@ -7,7 +7,7 @@ field_of_view     = 20*CONSTANTS_par["deg2rad_cst"]
 cos_field_of_view = np.cos(field_of_view)
 
 # STR orientation on BOF frame
-STRq_BOF = quaternions.rotvec2q(np.array([np.pi/2, 0, 0]))
+STRq_BOF = quaternions.rotvec2q(np.array([-np.pi/2, 0, 0]))
 
 # Parameters for Module SEN_STR
 SEN_STR_par = {

main/SEN/SEN_STR/Tests/test_SEN_STR.py
@@ -109,13 +109,13 @@ for step in range(1, n_steps):
     ###################
 
     # Spacecraft is aways pointing +STRz to Mars
-    # No roll allowed in the STR
     DYN_last_state = DYN_obj.snapshot()
     MARSpos_SSB = DYN_last_state["DYN_MARS"]["MARSpos_SSB"]
     SCpos_SSB   = DYN_last_state["DYN_TRA"]["SCpos_SSB"]
     STRy_STR    = np.array([0,1,0])
     STRz_STR    = np.array([0,0,1])
     STRq_BOF    = SEN_obj.SEN_STR.par["STRq_BOF"]
+
     BOFq_STR    = quaternions.qtrans(STRq_BOF)
 
     # Line of sight from SC to Mars
@@ -123,32 +123,10 @@ for step in range(1, n_steps):
     MARSdir_SC = SEN_obj.SEN_STR.dir_from_pos(MARSpos_SC)
 
     # STR boresight in the BOF frame
-    STRz_BOF = quaternions.qvecrot(STRz_STR, STRq_BOF)
+    STRz_BOF = quaternions.qvecprod(BOFq_STR, STRz_STR)
 
     # Get the quaternion which points STRz_BOF to MARSu_SSB
     BOFq_SSB_tgt = quaternions.vecqvec(STRz_BOF, MARSdir_SC)
-
-    # STRy in the SSB frame
-    SSBq_BOF = quaternions.qtrans(BOFq_SSB_tgt)
-    SSBq_STR = quaternions.qprod(SSBq_BOF, BOFq_STR)
-    STRy_SSB = quaternions.qvecrot(STRy_STR, SSBq_STR)
-
-    # Project STRy into the reference plane SSBz
-    SSBz = np.array([0,0,1])
-    STRy_proj  = STRy_SSB - np.dot(STRy_SSB, SSBz) * SSBz
-    STRy_proj /= np.linalg.norm(STRy_proj)
-
-    # Compute the roll correction quaternion
-    STRy_tgt   = np.array([0,1,0])
-    roll_axis  = MARSdir_SC
-    sin_ang    = np.dot(roll_axis, np.cross(STRy_proj, STRy_tgt))
-    cos_ang    = np.dot(STRy_proj, STRy_tgt)
-    roll_angle = np.arctan2(sin_ang, cos_ang)
-    roll_vec   = roll_angle * roll_axis
-    q_roll     = quaternions.rotvec2q(roll_vec)
-
-    # Final locked attitude
-    BOFq_SSB_tgt = quaternions.qprod(q_roll, BOFq_SSB_tgt)
 
     inputs["DYN"]["DYN_ATT"]["BOFq_SSB"] = BOFq_SSB_tgt
 
@@ -183,6 +161,7 @@ spice.clear_kernels()
 ###########
 
 field_of_view = SEN_obj.SEN_STR.par["field_of_view"]
+STRq_BOF      = SEN_obj.SEN_STR.par["STRq_BOF"]
 
 time_SIM          = timeline["DYN"]["DYN_TIME"]["time_SIM"]
 time_STR          = timeline["SEN"]["SEN_STR"]["time_STR"]
@@ -259,15 +238,26 @@ PPC.plot(MARSdir_SSB_mes[0,:],    MARSdir_SSB_mes[1,:],    MARSdir_SSB_mes[2,:],
 PPC.plot([0], [0], [0],  style='+', label="SC",  fig=fig, ax=ax, color=colors["magenta"])
 
 # Get axes in the SSB frame
+
 STRq_SSB = quaternions.qprod(STRq_BOF, BOFq_SSB)
 BOFx, BOFy, BOFz = quaternions.q2axis(BOFq_SSB)
 STRx, STRy, STRz = quaternions.q2axis(STRq_SSB)
+
+BOFx = BOFx[-1]
+BOFy = BOFy[-1]
+BOFz = BOFz[-1]
+
+PPC.plot([0, BOFx[0]], [0, BOFx[1]], [0, BOFx[2]], fig=fig, ax=ax, color=colors["blue"],  style='--', label="BOFx")
+PPC.plot([0, BOFy[0]], [0, BOFy[1]], [0, BOFy[2]], fig=fig, ax=ax, color=colors["red"],   style='--', label="BOFy")
+PPC.plot([0, BOFz[0]], [0, BOFz[1]], [0, BOFz[2]], fig=fig, ax=ax, color=colors["green"], style='--', label="BOFz")
 
 STRx = STRx[-1]
 STRy = STRy[-1]
 STRz = STRz[-1]
 
-PPC.plot([0, STRz[0]], [0, STRz[1]], [0, STRz[2]], fig=fig, ax=ax, color=colors["green"], label="STR boresight")
+PPC.plot([0, STRx[0]], [0, STRx[1]], [0, STRx[2]], fig=fig, ax=ax, color=colors["blue"],  label="STRx")
+PPC.plot([0, STRy[0]], [0, STRy[1]], [0, STRy[2]], fig=fig, ax=ax, color=colors["red"],   label="STRy")
+PPC.plot([0, STRz[0]], [0, STRz[1]], [0, STRz[2]], fig=fig, ax=ax, color=colors["green"], label="STRz")
 
 # Plot true vs measured Mars direction over time
 fig, ax = PPC.plot(time_SIM, MARSdir_SC.T, label=["x","y","z"], xlabel="time_SIM [s]", ylabel="MARSdir_SSB", title="Mars direction measurement")

main/Utils/quaternions.py
@@ -1,29 +1,14 @@
 import numpy as np
 
 # Quaternion product
-def qprod(a, b):
-    a = np.asarray(a)
-    b = np.asarray(b)
-    w1, x1, y1, z1 = np.moveaxis(a, -1, 0)
-    w2, x2, y2, z2 = np.moveaxis(b, -1, 0)
-    q = np.stack([
-        w1*w2 - x1*x2 - y1*y2 - z1*z2,
-        w1*x2 + x1*w2 + y1*z2 - z1*y2,
-        w1*y2 - x1*z2 + y1*w2 + z1*x2,
-        w1*z2 + x1*y2 - y1*x2 + z1*w2
-    ], axis=-1)
-    return q
-
-def qprod_ref(Aq_B, Bq_C):
+def qprod(Aq_B, Bq_C):
     Aq_B = np.asarray(Aq_B)
     Bq_C = np.asarray(Bq_C)
-    w1, x1, y1, z1 = np.moveaxis(Aq_B, -1, 0)
-    w2, x2, y2, z2 = np.moveaxis(Bq_C, -1, 0)
+    s_AB, x_AB, y_AB, z_AB = np.moveaxis(Aq_B, -1, 0)
+    s_BC, x_BC, y_BC, z_BC = np.moveaxis(Bq_C, -1, 0)
 
-    s_AB = w1
-    s_BC = w2
-    v_AB = np.stack([x1,y1,z1], axis=-1)
-    v_BC = np.stack([x2,y2,z2], axis=-1)
+    v_AB = np.stack([x_AB,y_AB,z_AB], axis=-1)
+    v_BC = np.stack([x_BC,y_BC,z_BC], axis=-1)
 
     s_AC = s_AB * s_BC - np.sum(v_AB * v_BC, axis=-1)
     v_AC = s_AB[..., None] * v_BC + s_BC[..., None] * v_AB - np.cross(v_AB, v_BC)
@@ -46,16 +31,16 @@ def qtrans(q):
     q_conj[..., 1:] *= -1
     return q_conj
 
-# Rotate vector by quaternion
-def qvecprod(Bq_A, v_A):
-    v_A = np.asarray(v_A)
-    Bq_A = np.asanyarray(Bq_A)
-    vq = np.zeros((*v_A.shape[:-1], 4))
-    vq[..., 1:] = v_A
-    Aq_B = qtrans(Bq_A)
-    vq_B = qprod(Aq_B, qprod(vq, Bq_A))
-    v_B = vq_B[..., 1:]
-    return v_B
+# Express in frame A a vector initially expressed in frame B
+def qvecprod(Aq_B, v_B):
+    Aq_B  = np.asarray(Aq_B)
+    v_B   = np.asarray(v_B)
+    Aq_B  = np.broadcast_to(Aq_B, v_B.shape[:-1] + (4,))
+    zeros = np.zeros(v_B.shape[:-1] + (1,))
+    q_vec = np.concatenate((zeros, v_B), axis=-1)
+    q_new = qprod(qprod(Aq_B, q_vec), qtrans(Aq_B))
+    v_A   = q_new[..., 1:4]
+    return v_A
 
 # Convert small angle vector to quaternion
 def rotvec2q(v):
@@ -103,18 +88,120 @@ def qchksign(q):
     sign = np.where(q[..., 0] < 0, -1, 1)
     return q*sign[..., None]
 
-# Return 3 axis vectors of the frame B relative to frame A
-def q2axis(Bq_A):
-    Bq_A = np.asarray(Bq_A)
+# Convert a quaternion to a cosine matrix
+def q2cosmat(Aq_B):
+    Aq_B = np.asarray(Aq_B)
 
-    xA = np.array([1,0,0])
-    yA = np.array([0,1,0])
-    zA = np.array([0,0,1])
+    w = Aq_B[..., 0]
+    x = Aq_B[..., 1]
+    y = Aq_B[..., 2]
+    z = Aq_B[..., 3]
 
-    Aq_B = qtrans(Bq_A)
+    C_AB = np.zeros(Aq_B.shape[:-1] + (3, 3))
 
-    xB = qvecprod(Aq_B, xA)
-    yB = qvecprod(Aq_B, yA)
-    zB = qvecprod(Aq_B, zA)
+    C_AB[..., 0, 0] = 2 * (w*w + x*x) - 1
+    C_AB[..., 0, 1] = 2 * (x*y + w*z)
+    C_AB[..., 0, 2] = 2 * (x*z - w*y)
 
-    return xB, yB, zB
+    C_AB[..., 1, 0] = 2 * (x*y - w*z)
+    C_AB[..., 1, 1] = 2 * (w*w + y*y) - 1
+    C_AB[..., 1, 2] = 2 * (y*z + w*x)
+
+    C_AB[..., 2, 0] = 2 * (x*z + w*y)
+    C_AB[..., 2, 1] = 2 * (y*z - w*x)
+    C_AB[..., 2, 2] = 2 * (w*w + z*z) - 1
+
+    return C_AB
+
+# Convert a cosine matrix to quaternion
+def cosmat2q(C_AB):
+    C_AB = np.asarray(C_AB)
+
+    b0 = 0.25 * (1.0 + np.trace(C_AB))
+    b1 = 0.25 * (1.0 + C_AB[0, 0] - C_AB[1, 1] - C_AB[2, 2])
+    b2 = 0.25 * (1.0 + C_AB[1, 1] - C_AB[0, 0] - C_AB[2, 2])
+    b3 = 0.25 * (1.0 + C_AB[2, 2] - C_AB[0, 0] - C_AB[1, 1])
+
+    b = np.array([b0, b1, b2, b3])
+    ind = np.argmax(b)
+
+    qvec = np.zeros(3)
+    qsca = 0.0
+
+    if ind == 0:
+        qsca = np.sqrt(b0)
+        qvec[0] = 0.25 * (C_AB[1, 2] - C_AB[2, 1]) / qsca
+        qvec[1] = 0.25 * (C_AB[2, 0] - C_AB[0, 2]) / qsca
+        qvec[2] = 0.25 * (C_AB[0, 1] - C_AB[1, 0]) / qsca
+    elif ind == 1:
+        qvec[0] = np.sqrt(b1)
+        qsca = 0.25 * (C_AB[1, 2] - C_AB[2, 1]) / qvec[0]
+        qvec[1] = 0.25 * (C_AB[0, 1] + C_AB[1, 0]) / qvec[0]
+        qvec[2] = 0.25 * (C_AB[2, 0] + C_AB[0, 2]) / qvec[0]
+    elif ind == 2:
+        qvec[1] = np.sqrt(b2)
+        qsca = 0.25 * (C_AB[2, 0] - C_AB[0, 2]) / qvec[1]
+        qvec[0] = 0.25 * (C_AB[0, 1] + C_AB[1, 0]) / qvec[1]
+        qvec[2] = 0.25 * (C_AB[1, 2] + C_AB[2, 1]) / qvec[1]
+    elif ind == 3:
+        qvec[2] = np.sqrt(b3)
+        qsca = 0.25 * (C_AB[0, 1] - C_AB[1, 0]) / qvec[2]
+        qvec[0] = 0.25 * (C_AB[2, 0] + C_AB[0, 2]) / qvec[2]
+        qvec[1] = 0.25 * (C_AB[1, 2] + C_AB[2, 1]) / qvec[2]
+
+    # Assemble quaternion: [scalar, vector]
+    Aq_B = np.hstack((qsca, qvec))
+
+    # Normalize and enforce sign convention
+    Aq_B = qnorm(Aq_B)
+    Aq_B = qchksign(Aq_B)
+
+    return Aq_B
+
+# Return 3 axis defining the A frame relative to B frame
+def q2axis(Aq_B):
+    C_AB = q2cosmat(Aq_B)
+    Ax_B = C_AB[..., 0, :]
+    Ay_B = C_AB[..., 1, :]
+    Az_B = C_AB[..., 2, :]
+    return Ax_B, Ay_B, Az_B
+
+# Convert roll, pitch and yaw to cosine matrix
+def rpy2cosmat(rpy):
+    rpy = np.asarray(rpy)
+
+    # Compute cos and sin
+    c1 = np.cos(rpy[0])
+    s1 = np.sin(rpy[0])
+    c2 = np.cos(rpy[1])
+    s2 = np.sin(rpy[1])
+    c3 = np.cos(rpy[2])
+    s3 = np.sin(rpy[2])
+
+    # Compute transformation matrix columns
+    x = np.array([
+        c2 * c3,
+        c2 * s3,
+        -s2
+    ])
+    y = np.array([
+        -c1 * s3 + s1 * s2 * c3,
+         c1 * c3 + s1 * s2 * s3,
+         s1 * c2
+    ])
+    z = np.array([
+         s1 * s3 + c1 * s2 * c3,
+        -s1 * c3 + c1 * s2 * s3,
+         c1 * c2
+    ])
+
+    # Direction cosine matrix
+    C = np.vstack((x, y, z))
+
+    return C
+
+# Convert roll, pitch and yaw to quaternion
+def rpy2q(rpy):
+    C = rpy2cosmat(rpy)
+    q = cosmat2q(C)
+    return q
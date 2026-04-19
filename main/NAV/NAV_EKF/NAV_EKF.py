d6fe6198128cbe46c11435df88b95d50cca1cee4
Mairon <maironwicz@gmail.com>
2026-04-19 14:56:10 -0300

    feat: NAV_PSR and NAV_CMB are looking good

--- main/NAV/NAV_EKF/NAV_EKF.py
+++ main/NAV/NAV_EKF/NAV_EKF.py
@@ -44,18 +44,18 @@ class NAV_EKF(Level2Module):
     def update_algebraic(self, t, SEN_states, NAV_states, inputs=None):
 
         navigation_methods = [
-            # {
-            #     "name" : "NAV_CEL",
-            #     "data" : NAV_states["NAV_CEL"],
-            # },
-            # {
-            #     "name" : "NAV_PSR",
-            #     "data" : NAV_states["NAV_PSR"],
-            # },
-            # {
-            #     "name" : "NAV_CMB",
-            #     "data" : NAV_states["NAV_CMB"],
-            # },
+            {
+                "name" : "NAV_CEL",
+                "data" : NAV_states["NAV_CEL"],
+            },
+            {
+                "name" : "NAV_PSR",
+                "data" : NAV_states["NAV_PSR"],
+            },
+            {
+                "name" : "NAV_CMB",
+                "data" : NAV_states["NAV_CMB"],
+            },
         ]
 
         x_est = self.state["x_est"]

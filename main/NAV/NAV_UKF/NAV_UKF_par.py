48e6c8a932a1437676f35885c439d2cf938534c4
Mairon <maironwicz@gmail.com>
2026-04-21 20:35:01 -0300

    feat: changed initial conditions for KF

--- main/NAV/NAV_UKF/NAV_UKF_par.py
+++ main/NAV/NAV_UKF/NAV_UKF_par.py
@@ -19,7 +19,7 @@ NAV_UKF_par = {
     "Q"         : Q,   # Process noise covariance
 
     # Initial values for the UKF
-    "x_est_ini" : np.array([1.62228612e+08, 2.42235379e+07, 1.78903662e+07, -0.32883677, 27.86282899, 13.43536119]) + np.array([1e7, 1e7, 1e7, 10, 10, 10]), # Initial state
+    "x_est_ini" : np.array([1.62228612e+08, 2.42235379e+07, 1.78903662e+07, -0.32883677, 27.86282899, 13.43536119]) + np.array([10, 10, 10, 1e-3, 1e-3, 1e-3]), # Initial state
     "P_ini"     : np.ones((6,6)), # Initial covariance
     "y_ini"     : 0, # Initial innovation
 }

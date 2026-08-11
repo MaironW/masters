fix: CSF quaternions

main/PPC/plots/NAV_plots.py
@@ -229,8 +229,8 @@ def NAV_CMB_plot(timeline, DYN_obj, SEN_obj, NAV_obj):
     fig, ax1 = PPC.plot([], [], ylabel="CMBR Temperature Dipole [K]", title="CMBR Temperature Dipole", subplot=(2,1,1))
     fig, ax2 = PPC.plot([], [], xlabel="time_SIM [s]", title="Standard deviation", ylabel="Covariance [K]", fig=fig, subplot=(2,1,2))
     for i in range(3):
-        PPC.plot(time_SIM, z[:, i],       label=f"CMB{i} meas.", fig=fig, ax=ax1)
-        PPC.plot(time_SIM, sigma_z[:, i], label=f"CMB{i} σ",     fig=fig, ax=ax2)
+        PPC.plot(time_SIM, z[:, i],       label=f"CMB{i+1} meas.", fig=fig, ax=ax1)
+        PPC.plot(time_SIM, sigma_z[:, i], label=f"CMB{i+1} σ",     fig=fig, ax=ax2)
 
     # Plot predicted measurement, assuming the true spacecraft velocity as the state + innovation
     x_true = np.hstack((SCpos_SSB, SCvel_SSB))
@@ -248,9 +248,9 @@ def NAV_CMB_plot(timeline, DYN_obj, SEN_obj, NAV_obj):
     fig, ax2 = PPC.plot([], [], xlabel="time_SIM [s]", ylabel="z - h(x)", title="Innovation z - h(x)", fig=fig, subplot=(2,1,2))
     innov = z - h_hist
     for i in range(3):
-        PPC.plot(time_SIM, h_hist[:, i], label=f"h(x) CMB{i}", fig=fig, ax=ax1)
-        PPC.plot(time_SIM, z[:, i], label=f"z CMB{i}", style='--', fig=fig, ax=ax1)
-        PPC.plot(time_SIM, innov[:, i], label=f"CMB{i}", xlabel="time_SIM [s]", fig=fig, ax=ax2)
+        PPC.plot(time_SIM, h_hist[:, i], label=f"h(x) CMB{i+1}", fig=fig, ax=ax1)
+        PPC.plot(time_SIM, z[:, i], label=f"z CMB{i+1}", style='--', fig=fig, ax=ax1)
+        PPC.plot(time_SIM, innov[:, i], label=f"CMB{i+1}", xlabel="time_SIM [s]", fig=fig, ax=ax2)
 
 def NAV_EKF_plot(timeline, DYN_obj, SEN_obj, NAV_obj):
     time_SIM  = timeline["DYN"]["DYN_TIME"]["time_SIM"]

main/SEN/SEN_CMB/SEN_CMB_par.py
@@ -3,9 +3,23 @@ from Utils import quaternions
 
 # Temperature sensors orientation on BOF frame (60 deg from spin axis, 120 deg apart)
 # CSF = CMB Sensor Frame
-CSF1q_BOF = quaternions.rotvec2q(np.pi/3 * np.array([0, 0, 1]))
-CSF2q_BOF = quaternions.rotvec2q(np.pi/3 * np.array([0, -np.sin(np.deg2rad(120)), +np.cos(np.deg2rad(120))]))
-CSF3q_BOF = quaternions.rotvec2q(np.pi/3 * np.array([0, -np.sin(np.deg2rad(240)), +np.cos(np.deg2rad(240))]))
+z_angle = np.deg2rad(-30) # [rad]
+y_angle = np.deg2rad(-60) # [rad]
+qz = quaternions.rotvec2q([0,0,z_angle])
+qy = quaternions.rotvec2q([0,y_angle,0])
+CSF1q_BOF = quaternions.qprod(qy,qz)
+
+z_angle = np.deg2rad(-150) # [rad]
+y_angle = np.deg2rad(-60) # [rad]
+qz = quaternions.rotvec2q([0,0,z_angle])
+qy = quaternions.rotvec2q([0,y_angle,0])
+CSF2q_BOF = quaternions.qprod(qy,qz)
+
+z_angle = np.deg2rad(-270) # [rad]
+y_angle = np.deg2rad(-60) # [rad]
+qz = quaternions.rotvec2q([0,0,z_angle])
+qy = quaternions.rotvec2q([0,y_angle,0])
+CSF3q_BOF = quaternions.qprod(qy,qz)
 
 # Parameters for Module SEN_CMB
 SEN_CMB_par = {
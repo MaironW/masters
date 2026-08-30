74ac49aebf3a6c0277c859fb44167c57271fd129
MaironW <maironwicz@gmail.com>
2026-08-30 16:24:06 -0300

    feat: update CMB par

--- main/SEN/SEN_CMB/SEN_CMB_par.py
+++ main/SEN/SEN_CMB/SEN_CMB_par.py
@@ -24,9 +24,9 @@ CSF3q_BOF = quaternions.qprod(qy,qz)
 # Parameters for Module SEN_CMB
 SEN_CMB_par = {
     # Temperature detector parameters
-    "dt"         : 60,        # [s] Discretization of the sensor time - Also observation time
+    "dt"         : 320,       # [s] Discretization of the sensor time - Also observation time
     "noise_mean" : 0,         # [K] Sensor noise mean
-    "noise_std"  : 100e-6,    # [K] Sensor noise std
+    "noise_std"  : 33.7e-6,   # [K] Sensor noise std
     "CSF1q_BOF"  : CSF1q_BOF, # Orientation of the CMB sensor 1 relative to the Spacecraft body
     "CSF2q_BOF"  : CSF2q_BOF, # Orientation of the CMB sensor 2 relative to the Spacecraft body
     "CSF3q_BOF"  : CSF3q_BOF, # Orientation of the CMB sensor 3 relative to the Spacecraft body
